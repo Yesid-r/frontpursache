2b26d388d8b7875b49cda9af1d62bacb92b452e3
Yesid Rincon <dairo.rincon@uptc.edu.co>
2024-04-26 15:03:40 -0500

    update table

--- src/components/TableDelivery.jsx
+++ src/components/TableDelivery.jsx
@@ -51,7 +51,7 @@ const TableDelivery = () => {
 
     return (
         <div className="overflow-x-auto">
-            <table className="min-w-full divide-y divide-gray-200">
+            <table className="max-w-lg mx-auto m-3 rounded-md divide-y divide-gray-200">
                 <thead>
                     <tr>
                         <th className="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cliente</th>
